0e2d5541adffb68ded9db0ff56517407587c84e3
Vadim Slepich <35778844+d3fault33@users.noreply.github.com>
2018-05-04 18:42:57 +0300

    Update copy_replace.bat
    Script for copying contents of some directory into another directory with replacing files

--- copy_replace.bat
+++ copy_replace.bat
@@ -4,7 +4,7 @@
 @set dest=\\s527-card\Work\cirrus\*
 
 rem adding windows system dir to path variable due to starting this script on Winserver 
-rem with UAC enabled under admin account
+rem with UAC enabled under admin account 
 set path=C:\Windows\System32\;%oldpath%
 
 xcopy %src% %dest% /E /Y /H /R /C >> D:\copylog.txt
