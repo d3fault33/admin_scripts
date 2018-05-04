843efe2d318bc661a969c8434d0d7bef9de6a282
Vadim Slepich <35778844+d3fault33@users.noreply.github.com>
2018-05-04 18:31:43 +0300

    Create copy_replace.bat

---
+++ copy_replace.bat
@@ -0,0 +1,10 @@
+@echo on
+@set src=\\s527-fs02\work\VERSII\_softclu\SCBKI\2017\rask\*
+
+@set dest=\\s527-card\Work\cirrus\*
+
+rem adding windows system dir to path variable due to starting this script on Winserver 
+rem with UAC enabled under admin account
+set path=C:\Windows\System32\;%oldpath%
+
+xcopy %src% %dest% /E /Y /H /R /C >> D:\copylog.txt
